6a263534daeba653c384fda8c271de2132db5c08
Rajendrankr25 <rajendrankr25@gmail.com>
2022-12-30 11:01:37 +0530

    organize movies with route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express'; // latest export method
 import { MongoClient } from 'mongodb';
 import * as dotenv from 'dotenv';
+import movieRouter from "./routes/movies.route.js" //importing routes
 dotenv.config()
 
 const app = express(); // calling the express package
@@ -23,76 +24,8 @@ app.get("/", function (request, response) { //get method
     response.send("This is the home page 😍😍😍😍");
 });
 
-//Task 1 - list movie data @http://localhost:4000/movies
-// app.get("/movies", function (request, response) { //get method
-//     response.send(movies);
-// });
-
-//Task 2 - get movies based on movie id
-// app.get("/movies/:id", function (request, response) { //get method
-//     const { id } = request.params;
-//     console.log(id);
-//     //const movie = movies.filter((mv) => mv.id == id); // 1st method filter
-//     const movie = movies.find((mv) => mv.id == id); //find methods returns only 1 element
-//     movie ? response.send(movie) : response.status(404).send({ message: "movie not found" });
-// });
-
-// get data from mongo db; Mongodb & node, express connection
-app.get("/movies/:id", async function (request, response) { //get method
-    const { id } = request.params;
-    const movie = await client.db('mongoTest').collection('movies').findOne({ id: id }); //accessing data from mongodb
-    movie ? response.send(movie) : response.status(404).send({ message: "movie not found" });
-});
-
-//POST movies from postman - create/insert movies
-app.post("/movies", async function (request, response) { //post method
-    const data = request.body;
-    console.log(data);
-    //db.movies.insertMany(data)
-    const result = await client.db("mongoTest").collection("movies").insertMany(data);
-    response.send(result);
-});
-
-// find all movies from mongodb
-// app.get("/movies", async function (request, response) { //get method
-//     //db.movies.find({})
-//     const movies = await client.db("mongoTest").collection("movies").find({}).toArray(); //toarray returns all data instead of cursor-paginated data
-//     response.send(movies);
-// });
-
-//Delete method
-app.delete("/movies/:id", async function (request, response) { //delete method
-    const { id } = request.params;
-    const result = await client.db('mongoTest')
-        .collection('movies')
-        .deleteOne({ id: id }); //deleting data from mongodb
-    console.log(result);
-    result.deletedCount > 0 ? response.send({ message: "movie deleted" }) :
-        response.send({ message: "movie not found" });
-});
-
-//PUT/UPDATE method
-app.put("/movies/:id", async function (request, response) { //put method
-    //db.movies.updateOne({id:'99}, {$set:{rating:9}})
-    const { id } = request.params;
-    const data = request.body;
-    const result = await client.db('mongoTest')
-        .collection('movies')
-        .updateOne({ id: id }, { $set: data }); //updating data from mongodb
-    console.log(result);
-    response.send(result);
-});
-
-//query params
-app.get("/movies", async function (request, response) { //get method
-    if (request.query.rating) {
-        request.query.rating = +request.query.rating;
-    }
-    const movies = await client.db("mongoTest")
-        .collection("movies")
-        .find(request.query)
-        .toArray(); //toarray returns all data instead of cursor-paginated data
-    response.send(movies);
-});
+app.use("/movies", movieRouter);
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
+
+export { client }; //exporting client to routes
